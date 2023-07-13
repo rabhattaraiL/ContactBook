f3be04899cc3210a277a665a7eb9643320f7da0e
Rasa Bhattarai <bhattarairasa@gmail.com>
2023-07-13 14:49:23 +0545

    Update script.sql

--- scripts/script.sql
+++ scripts/script.sql
@@ -6,8 +6,8 @@ BEGIN
         contact_id INT IDENTITY(1,1) PRIMARY KEY,
         first_name NVARCHAR(20), 
         last_name NVARCHAR(20),
-		email NVARCHAR(50),
-		phone_number NVARCHAR(15)
+	email NVARCHAR(50),
+	phone_number NVARCHAR(15)
     )
 END
 
@@ -17,12 +17,12 @@ BEGIN
     IF NOT EXISTS (SELECT * FROM sys.tables WHERE name='AddressBook')
     CREATE TABLE AddressBook (
         address_id INT IDENTITY(1,1) PRIMARY KEY,
-		contact_id INT FOREIGN KEY REFERENCES ContactTable(contact_id) ON DELETE CASCADE,
+	contact_id INT FOREIGN KEY REFERENCES ContactTable(contact_id) ON DELETE CASCADE,
         street_address NVARCHAR(20), 
         city NVARCHAR(20),
-		state NVARCHAR(50),
-		postal_code NVARCHAR(15),
-		country NVARCHAR(20),
+	state NVARCHAR(50),
+	postal_code NVARCHAR(15),
+	country NVARCHAR(20),
     )
 END
 
